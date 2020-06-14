94e651d8080972746aa7095a3fbfd3d3a4678f94
minky97 <leemkl@ajou.ac.kr>
2020-06-15 00:07:21 +0900

    remove when no journal

--- tools/autotools/tsk_recover.cpp
+++ tools/autotools/tsk_recover.cpp
@@ -358,9 +358,7 @@ TSK_RETVAL_ENUM TskRecover::processFile(TSK_FS_FILE * fs_file, const char *path)
             if (fs_file->meta->time2.ext2.dtime >= recover_time || fs_file->meta->atime >= recover_time)
             {
                 EXT2FS_INFO* ext2fs = (EXT2FS_INFO*)fs_file->fs_info;
-                if(ext4_jrecover(fs_file->fs_info, fs_file->meta, fs_file->meta->addr)){
-                   
-                }
+                ext4_jrecover(fs_file->fs_info, fs_file->meta, fs_file->meta->addr);
                 if (m_reportOption)
                 {
                     uint32_t recover_grp = (fs_file->meta->addr - 1) / tsk_getu32(fs_file->fs_info->endian, ext2fs->fs->s_inodes_per_group);
